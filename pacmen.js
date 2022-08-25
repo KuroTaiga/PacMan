59465454fb7bf2ec99c80e3703e96979fbfd8091
Dong <jdong@ccanet.rockwellcollins.com>
2022-08-24 21:41:16 -0500

    Updated to include different facings and the eating animation

--- pacmen.js
+++ pacmen.js
@@ -3,7 +3,7 @@ const pacArray = [
   ['./images/PacMan1.png', './images/PacMan2.png'],
   ['./images/PacMan3.png', './images/PacMan4.png'],
 ];
-let direction = 0;
+var focus =0;
 const pacMen = []; // This array holds all the pacmen
 
 // This function returns an object with random values
@@ -43,8 +43,12 @@ function makePac() {
 
 function update() {
   // loop over pacmen array and move each one and move image in DOM
+  focus = (focus+1)%2;
   pacMen.forEach((item) => {
+	let direction = 0;
     checkCollisions(item);
+	if(item.velocity.x<0) direction =1;
+	item.newimg.src = pacArray[direction][focus];
     item.position.x += item.velocity.x;
     item.position.y += item.velocity.y;
 
